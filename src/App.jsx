644fceef4e76af3d863f79acaa4080abedbe3a75
Jubayer Ahmed <web.ahjubayer@gmail.com>
2023-10-27 15:09:14 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="header">
+        <h2> learning</h2>
+        <h1>React Folder structure</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
